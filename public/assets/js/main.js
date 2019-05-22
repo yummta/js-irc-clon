a8a20286ae6fdb76f6c082878a13ae63d00ca00b
cristianbgp <cristian.granda.pastor@gmail.com>
2019-05-22 15:42:20 -0500

    Create join function and changeActiveChannel

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,12 +2,14 @@ console.log("Cool");
 
 let channels = { general: { name: "general" } };
 let userChannels = ["general"];
+let activeChannel = channels["general"];
 
 function createChannel(channelName) {
   const channelCreated = { channelName: { name: channelName } };
   const channelExists = channels.hasOwnProperty(channelName);
   if (!channelExists) {
     channels[channelName] = channelCreated.channelName;
+    joinChannel(channelName);
     return channels;
   }
 }
@@ -15,5 +17,14 @@ function createChannel(channelName) {
 function joinChannel(channelName) {
   if (!userChannels.includes(channelName)) {
     userChannels.push(channelName);
+    return userChannels;
+  }
+}
+
+function changeActiveChannel(channelName) {
+  const channelExists = userChannels.includes(channelName);
+  if (channelExists) {
+    activeChannel = channels[channelName];
+    return activeChannel;
   }
 }
