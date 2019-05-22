ce5e62e53abf50441b72f8b08ae51f089af5d7ba
paul <yummta@gmail.com>
2019-05-22 12:21:32 -0500

    Add login and save user in localStorage

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,13 @@
-console.log("Cool")
+const $buttonLogin = document.getElementById("js-button-loggin")
+const $inputUsername = document.getElementById("js-input-username")
+
+$buttonLogin.addEventListener( "click", login )
+
+function login() {
+  event.preventDefault()
+  const username = $inputUsername.value
+  if ( username ) {
+    localStorage.setItem("username", username);
+    console.log(`[Save username ${username} in localStorage]`)
+  }
+}
