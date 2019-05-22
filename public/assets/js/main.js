620dbb523430ad02d7660458195bc747c28140ee
Cristian Granda <cristian.granda.pastor@gmail.com>
2019-05-22 16:34:08 -0500

    Channels usability (#13)
    * Add create and join functions to the channels
    
    * Create join function and changeActiveChannel
    
    * Add call for changeActiveChannel inside the createChannel

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,13 +1,42 @@
-const $buttonLogin = document.getElementById("js-button-loggin")
-const $inputUsername = document.getElementById("js-input-username")
+const $buttonLogin = document.getElementById("js-button-loggin");
+const $inputUsername = document.getElementById("js-input-username");
 
-$buttonLogin.addEventListener( "click", login )
+$buttonLogin.addEventListener("click", login);
 
 function login() {
-  event.preventDefault()
-  const username = $inputUsername.value
-  if ( username ) {
+  event.preventDefault();
+  const username = $inputUsername.value;
+  if (username) {
     localStorage.setItem("username", username);
-    console.log(`[Save username ${username} in localStorage]`)
+    console.log(`[Save username ${username} in localStorage]`);
+  }
+}
+
+let channels = { general: { name: "general" } };
+let userChannels = ["general"];
+let activeChannel = channels["general"];
+
+function createChannel(channelName) {
+  const channelCreated = { channelName: { name: channelName } };
+  const channelExists = channels.hasOwnProperty(channelName);
+  if (!channelExists) {
+    channels[channelName] = channelCreated.channelName;
+    joinChannel(channelName);
+    changeActiveChannel(channelName);
+    return channels;
+  }
+}
+
+function joinChannel(channelName) {
+  if (!userChannels.includes(channelName)) {
+    userChannels.push(channelName);
+    return userChannels;
+  }
+}
+
+function changeActiveChannel(channelName) {
+  const channelExists = userChannels.includes(channelName);
+  if (channelExists) {
+    return (activeChannel = channels[channelName]);
   }
 }
