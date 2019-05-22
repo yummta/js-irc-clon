5699b9a0c1649b61298b8ba2da44da7c4f8a27bd
mayra navarro <luciagirasoles@gmail.com>
2019-05-22 16:46:14 -0500

    return  time and date formatted

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -40,3 +40,113 @@ function changeActiveChannel(channelName) {
     return (activeChannel = channels[channelName]);
   }
 }
+
+//new Date(year, month, date, hours, minutes, seconds, ms)
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutes + " " + ampm;
+  return strTime;
+}
+
+function formatdate(someDate) {
+  let FormattedDate = "nada";
+  let today = new Date();
+  let yesterday = new Date();
+  yesterday.setDate(today.getDate() - 1);
+
+  if (
+    someDate.getDate() == today.getDate() &&
+    someDate.getMonth() == today.getMonth() &&
+    someDate.getFullYear() == today.getFullYear()
+  ) {
+    FormattedDate = "Today";
+  } else if (
+    someDate.getDate() == yesterday.getDate() &&
+    someDate.getMonth() == yesterday.getMonth() &&
+    someDate.getFullYear() == yesterday.getFullYear()
+  ) {
+    FormattedDate = "Yesterday";
+  } else {
+    FormattedDate = `${getWeekDay(someDate)}, ${getMonth(
+      someDate
+    )} ${getformatDay(someDate)} `;
+  }
+  // return Today, yesterday, "Monday, May 20th"
+  return FormattedDate;
+}
+
+function getWeekDay(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thrusday",
+    "Friday",
+    "Saturday"
+  ];
+
+  return days[date.getDay()];
+}
+
+function getMonth(date) {
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+
+  return months[date.getMonth()];
+}
+
+function getformatDay(date) {
+  let ordinals = [
+    "",
+    "st",
+    "nd",
+    "rd",
+    "th",
+    "th",
+    "th",
+    "th",
+    "th",
+    "th",
+    "th",
+    "th",
+    "th",
+    "th",
+    "th",
+    "th",
+    "th",
+    "th",
+    "th",
+    "th",
+    "th",
+    "st",
+    "nd",
+    "rd",
+    "th",
+    "th",
+    "th",
+    "th",
+    "th",
+    "th",
+    "th",
+    "st"
+  ];
+
+  return `${date.getDate()}${ordinals[date.getDate()]}`;
+}
