adc8532ed8043d420792df1e3321f570415207f0
cristianbgp <cristian.granda.pastor@gmail.com>
2019-05-22 12:44:20 -0500

    Add create and join functions to the channels

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,19 @@
-console.log("Cool")
+console.log("Cool");
+
+let channels = { general: { name: "general" } };
+let userChannels = ["general"];
+
+function createChannel(channelName) {
+  const channelCreated = { channelName: { name: channelName } };
+  const channelExists = channels.hasOwnProperty(channelName);
+  if (!channelExists) {
+    channels[channelName] = channelCreated.channelName;
+    return channels;
+  }
+}
+
+function joinChannel(channelName) {
+  if (!userChannels.includes(channelName)) {
+    userChannels.push(channelName);
+  }
+}
