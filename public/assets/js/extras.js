c294753100475337f12d6dc51937f9137113312e
mayra navarro <luciagirasoles@gmail.com>
2019-05-23 17:46:58 -0500

    avoid weird characters in user messages

--- public/assets/js/extras.js
+++ public/assets/js/extras.js
@@ -4,3 +4,9 @@ function lastLine() {
     "js-messages-list"
   ).scrollTop = document.getElementById("js-messages-list").scrollHeight;
 }
+
+//avoid weird characters
+document.getElementById("js-input-user-message").onkeypress = function(e) {
+  var chr = String.fromCharCode(e.which);
+  if ("></".indexOf(chr) >= 0) return false;
+};
