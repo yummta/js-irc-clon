d06c27340833eb879bdc351b0ba03b23ce924199
Carlos Sandoval <iznomag@gmail.com>
2019-05-23 09:46:35 -0500

    Feature/storage show messages (#24)
    * First Connection
    
    * Add sockect js
    
    * Refactoring
    
    * Save and show in both clients

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,24 +1,34 @@
 const $inputUsername = document.getElementById("js-input-username");
 const $buttonLogin = document.getElementById("js-button-loggin");
+let socket = new WebSocket("ws://localhost:3000");
 
 function login() {
   event.preventDefault();
   const username = $inputUsername.value;
+  let new_data_user = {
+    user: username,
+    channel: ["general"],
+    message: {
+      general: {
+        messages: []
+      }
+    }
+  };
+
   if (username) {
-    localStorage.setItem("username", username);
+    localStorage.setItem("data", JSON.stringify(new_data_user));
     window.location.replace("/main");
   }
 }
 
-if ( !localStorage.getItem("username") ) {
-  if ( window.location.pathname !== "/" ) {
+if (typeof localStorage.getItem("data") == "object") {
+  if (window.location.pathname !== "/") {
     window.location.replace("/");
   } else {
     $buttonLogin.addEventListener("click", login);
   }
 } else {
-  if ( window.location.pathname !== "/main" ) {
-    console.log("eee")
+  if (window.location.pathname !== "/main") {
     window.location.replace("/main");
   }
 }
